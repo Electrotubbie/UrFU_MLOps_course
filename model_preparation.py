0dccd7e2cf603f1ae190206c5cdf27ef7c56d7da
Земов Василий Александрович <v.zemov@prosoftsystems.ru>
2024-03-29 23:28:25 +0300

    Added model_preparation.py

--- model_preparation.py
+++ model_preparation.py
@@ -1,1 +1,13 @@
-print('model_preparation')
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import pickle
+
+X_train, y_train = np.load('./train/X_train.npy'), np.load('./train/y_train.npy')
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
+
--- model_testing.py
+++ model_testing.py
@@ -1,1 +1,14 @@
-print('model_testing')
+import numpy as np
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error as mse
+import pickle
+
+X_test, y_test = np.load('./test/X_test.npy'), np.load('./test/y_test.npy')
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+y_test_pred = model.predict(X_test)
+
+print(f'r2: {r2_score(y_test, y_test_pred)}')
+print(f'mse: {mse(y_test, y_test_pred, squared=False)}')
