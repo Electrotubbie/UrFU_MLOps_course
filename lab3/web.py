b63fee94bebd6e74417095b5d0a94afeeccb0f45
Electrotubbie <steen2033@yandex.ru>
2024-06-02 16:04:20 +0500

    change model and requirements for it

--- lab3/web.py
+++ lab3/web.py
@@ -1,13 +1,13 @@
 from transformers import pipeline
 import streamlit as st
 
-classifier = pipeline("translation", model="facebook/nllb-200-distilled-600M")
-st.title('Перевод текста на английский язык')
-text = st.text_input('Введите текст на русском языке')
-result = st.button('Нажми, что бы перевести!')
+classifier = pipeline("text-classification", model="papluca/xlm-roberta-base-language-detection")
+st.title('Определитель языка')
+text = st.text_input('Введите текст:')
+result = st.button('Нажми, что бы определить!')
 if result:
     text_eng = classifier(text)
-    st.subheader('Перевод:')
-    st.markdown(text_eng[0]['translation_text'])
+    st.subheader('Язык, на котором написан текст:')
+    st.markdown(text_eng)
 else:
-    st.write('Ожидание текста для перевода')
+    st.write('Ожидание текста для определения языка')
